7f3b1dd1339ec2f0cf27ad2fe5f40f9bcd07e2ac
ryansldev <ryanlima1309@gmail.com>
2022-10-27 15:53:18 -0300

    feat: update firebase config

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,18 +1,18 @@
-import firebase from 'firebase/app';
+import * as firebase from "firebase/app";
 
-import { getDatabase } from "firebase/database";
+import { getDatabase, ref, push, update, child, onValue } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID
 };
 
 const app = firebase.initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-export { firebase, database };
+export { firebase, database, ref, push, update, child, onValue };
