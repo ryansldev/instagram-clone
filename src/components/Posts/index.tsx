000cb9bd512dfc2ca5e11d6a75223c739e4aa3b4
ryansldev <ryanlima1309@gmail.com>
2022-10-28 10:59:12 -0300

    refactor: updating posts component

--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -1,10 +1,37 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { PostType } from "../../@types/Post";
 import Post from "./Post";
 import styles from "./index.module.css";
 import PostModal from "./PostModal";
+import { database, ref, onValue } from "../../services/firebase";
 
-const Posts: FC<{ posts: PostType[] }> = ({ posts }) => {
+type FirebasePosts = Record<string, PostType>;
+
+const Posts: FC = () => {
+  const [posts, setPosts] = useState<PostType[]>([]);
+  useEffect(() => {
+    const postsRef = ref(database, 'posts');
+    onValue(postsRef, (snapshot) => {
+      const data: FirebasePosts = snapshot.val();
+      if(data) {
+        const parsedData: PostType[] = Object.entries(data).map(([key, value]) => {
+          return {
+            id: key,
+            author: value.author,
+            username: value.username,
+            title: value.title,
+            photo: value.photo,
+            likes: value.likes,
+            postedAt: value.postedAt,
+            comments: value.comments,
+          }
+        });
+
+        setPosts(parsedData);
+      }
+    });
+  }, []);
+
   return (
     <div className={styles.posts}>
       {posts && posts.map((post, key) => (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,33 +4,7 @@ import { PostType } from '../@types/Post';
 import Posts from '../components/Posts'
 import { database, ref, onValue } from "../services/firebase";
 
-type FirebasePosts = Record<string, PostType>;
-
 export default function Home() {
-  const [posts, setPosts] = useState<PostType[]>([]);
-  useEffect(() => {
-    const postsRef = ref(database, 'posts');
-    onValue(postsRef, (snapshot) => {
-      const data: FirebasePosts = snapshot.val();
-      if(data) {
-        const parsedData: PostType[] = Object.entries(data).map(([key, value]) => {
-          return {
-            id: key,
-            author: value.author,
-            username: value.username,
-            title: value.title,
-            photo: value.photo,
-            likes: value.likes,
-            postedAt: value.postedAt,
-            comments: value.comments,
-          }
-        });
-
-        setPosts(parsedData);
-      }
-    });
-  }, []);
-
   return (
     <main>
       <Head>
@@ -40,9 +14,7 @@ export default function Home() {
       </Head>
 
       <div>
-        <Posts
-          posts={posts}
-        />
+        <Posts />
       </div>
     </main>
   )
